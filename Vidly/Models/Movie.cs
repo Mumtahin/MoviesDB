81112cc81ea10ca6d732a979e5aa96e60fe64c85
Mumtahin <n.mumtahin1@gmail.com>
2017-02-07 02:19:45 +0000

    Project Status Completing Section 4 Exercise - Building Forms
    A Movie form was added into the application allowing user to add movies
    to the database. Also users are now able to edit/update existing records
    in the database.

--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.ComponentModel.DataAnnotations;
 
 namespace Vidly.Models
@@ -16,9 +13,17 @@ namespace Vidly.Models
 
         [Required]
         public Genre Genre { get; set; }
+
+        [Display(Name = "Genre")]
+        [Required]
         public byte GenreId { get; set; }
+
         public DateTime DateAdded { get; set; }
+
+        [Display(Name = "Release Date")]
         public DateTime ReleaseDate { get; set; }
+
+        [Display(Name = "Number in Stock")]
         public byte NumberInStock { get; set; }
     }
 }
